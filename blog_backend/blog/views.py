6b6697eda06080f814d19eb02aca2b6bb04df261
bodaghialib4 <bodaghialib4@gmail.com>
2020-10-14 12:32:15 +0330

    add PostViewSet in views.py (back-end)

--- blog_backend/blog/views.py
+++ blog_backend/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import  ModelViewSet
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [AllowAny]
